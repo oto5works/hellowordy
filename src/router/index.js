46b9e2f6fd9ee5a749711edc1959d49f907e322d
JW Park <jwpg@kakao.com>
2024-04-29 19:49:33 +0900

    vx

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,28 @@
 // router/index.js
 
-
-
-import { createRouter, createWebHashHistory } from 'vue-router';
-import main from '@/views/main.vue';
-import viewWords from '@/views/viewWords.vue';
-import addwords from '@/views/addwords.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import main from "@/views/main.vue";
+import viewWords from "@/views/viewWords.vue";
+import add from "@/views/add.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
-      path: '/',
-      name: 'main',
+      path: "/",
+      name: "main",
       component: main,
     },
 
-    
     {
-      path: '/viewWords',
-      name: 'viewWords',
+      path: "/viewWords",
+      name: "viewWords",
       component: viewWords,
     },
     {
-      path: '/addwords',
-      name: 'addwords',
-      component: addwords,
+      path: "/add",
+      name: "add",
+      component: add,
     },
   ],
 });
