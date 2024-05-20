232597e34361f5c417cff77671dbeef147e30aa4
JW Park <jwpg@kakao.com>
2024-05-20 19:03:25 +0900

    google ad

--- src/components/adComponent.vue
+++ src/components/adComponent.vue
@@ -2,7 +2,7 @@
   <div class="ad-container">
     <!-- AdSense 광고 코드를 여기에 삽입합니다. -->
     <ins class="adsbygoogle"
-         style="display:block"
+    style="display:inline-block;width:320px;height:50px"
          data-ad-client="ca-pub-3365711295624526"
          data-ad-slot="4074778003"
          data-ad-format="auto"
